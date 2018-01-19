9edf09b6230c51ba3ed729cc4126338de234c087
Diego Pessoa <dabpessoa@gmail.com>
2018-01-19 12:17:17 -0300

    Correção injeção de dependencia para itensdominio.

--- src/main/java/br/com/digitoglobal/util/util/itemDominio/IItemDominio.java
+++ src/main/java/br/com/digitoglobal/util/util/itemDominio/IItemDominio.java
@@ -2,16 +2,16 @@ package br.com.digitoglobal.util.util.itemDominio;
 
 import br.com.digitoglobal.util.bean.model.ItemDominio;
 import br.com.digitoglobal.util.service.ItemDominioService;
-import br.com.digitoglobal.util.util.SpringUtils;
+import me.dabpessoa.framework.service.SpringContextProvider;
 
 public interface IItemDominio {
 	ItemDominio getItem(boolean deveBuscarDoBancoDeDados);
 	ItemDominio getItem();
 	void preencherItemAPartirDoBancoDeDados();
 
-	default ItemDominio buscarNoBancoDeDados(String label, String nomeDominio, String nomeModulo) {
+	default ItemDominio buscarNoBancoDeDados(String label, String nomeDominio, String nomeModulo, SpringContextProvider springContextProvider) {
 		if (label == null) return null;
-		ItemDominioService itemDominioService = SpringUtils.getBean(ItemDominioService.class);
+		ItemDominioService itemDominioService = springContextProvider.getBean(ItemDominioService.class);
 		return itemDominioService.findByLabelAndDominioAndModulo(label, nomeDominio, nomeModulo);
 	}
 
